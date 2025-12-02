54dd01e13df829050a6658046cb8ad8180d105b8
Tsinswreng <Tsinswreng@gmail.com>
2025-12-02 23:27:01 +0800

    Update IWebAns.cs

--- proj/Tsinswreng.CsErr/IWebAns.cs
+++ proj/Tsinswreng.CsErr/IWebAns.cs
@@ -1,6 +1,7 @@
 namespace Tsinswreng.CsErr;
 
 using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
 
 
 /// <summary>
@@ -27,30 +28,6 @@ public class WebAns:WebAns<obj>
 		};
 	}
 
-#if false
-	public static IWebAns<T> Deserialize<T>(str Json){
-		using var doc = JsonDocument.Parse(Json);
-		var root = doc.RootElement;
-
-		// 1. 反序列化 Errors（始终用源生成器）
-		IList<AppErrView>? errors = null;
-		if (root.TryGetProperty(nameof(Errors), out var errEl) && errEl.ValueKind != JsonValueKind.Null){
-			errors = errEl.Deserialize(AppJsonCtx.Default.IListAppErrView);
-		}
-
-		// 2. 反序列化 Data（按需用默认源生成器或调用方提供的选项）
-		T? data = default;
-		if (root.TryGetProperty(nameof(Data), out var dataEl) && dataEl.ValueKind != JsonValueKind.Null){
-			//“AppJsonCtx”未包含“GetRequiredTypeInfo”的定义，并且找不到可接受第一个“AppJsonCtx”类型参数的可访问扩展方法“GetRequiredTypeInfo”(是否缺少 using 指令或程序集引用?)CS1061
-			data = (T?)dataEl.Deserialize(AppJsonCtx.Default.GetTypeInfo(typeof(T))!);
-		}
-
-		var R = new WebAns<T>();
-		R.Data = data;
-		R.Errors = errors?.Select(x=>(IAppErrView)x).ToList();
-		return R;
-	}
-#endif
 }
 
 public static class ExtnWebAns{
@@ -62,5 +39,37 @@ public static class ExtnWebAns{
 		}
 		return z.Data;
 	}
+
+
+	extension(WebAns z){
+		public static IWebAns<T> Deserialize<T>(
+			str Json
+			,JsonTypeInfo<IList<AppErrView>> IListAppErrViewJsonTypeInfo
+			,Func<Type, JsonTypeInfo> FnGetJsonTypeInfo
+		){
+			using var doc = JsonDocument.Parse(Json);
+			var root = doc.RootElement;
+			// 1. 反序列化 Errors（始终用源生成器）
+			IList<AppErrView>? errors = null;
+			if (root.TryGetProperty(nameof(IWebAns<>.Errors), out var errEl) && errEl.ValueKind != JsonValueKind.Null){
+				errors = errEl.Deserialize(IListAppErrViewJsonTypeInfo);
+			}
+
+			// 2. 反序列化 Data（按需用默认源生成器或调用方提供的选项）
+			T? data = default;
+			if (root.TryGetProperty(nameof(IWebAns<>.Data), out var dataEl) && dataEl.ValueKind != JsonValueKind.Null){
+				//“AppJsonCtx”未包含“GetRequiredTypeInfo”的定义，并且找不到可接受第一个“AppJsonCtx”类型参数的可访问扩展方法“GetRequiredTypeInfo”(是否缺少 using 指令或程序集引用?)CS1061
+				data = (T?)dataEl.Deserialize(FnGetJsonTypeInfo(typeof(T))!);
+			}
+
+			var R = new WebAns<T>();
+			R.Data = data;
+			R.Errors = errors?.Select(x=>(IAppErrView)x).ToList();
+			return R;
+		}
+
+
+	}
+
 
 }
